0ba4fbfeea4135a428f6d47b56c64ec8ab512571
Emin Šiljak <esiljak1@etf.unsa.ba>
2020-02-11 00:05:43 +0100

    Added default image for the signup

--- src/ba/unsa/etf/rpr/projekat/SignUpController.java
+++ src/ba/unsa/etf/rpr/projekat/SignUpController.java
@@ -1,10 +1,21 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
 public class SignUpController {
+    public ImageView imgAvatar;
 
     public SignUpController() {
     }
-    //todo dodati default sliku za signup
+    @FXML
+
+    public void initialize(){
+        imgAvatar.setImage(new Image(new File("default.jpg").toURI().toString(), 100, 100, false, false));
+    }
 
     //todo mrežno programiranje, koristiti giphy za postavljanje slika :)
 }
