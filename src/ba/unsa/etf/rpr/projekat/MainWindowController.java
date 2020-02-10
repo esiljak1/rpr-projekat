a496e14c4ba647c5aea8b5965a6f9de4c86086f4
Emin Šiljak <esiljak1@etf.unsa.ba>
2020-02-10 23:19:25 +0100

    Added login screen without functionalities

--- src/ba/unsa/etf/rpr/projekat/MainWindowController.java
+++ src/ba/unsa/etf/rpr/projekat/MainWindowController.java
@@ -1,7 +1,32 @@
 package ba.unsa.etf.rpr.projekat;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainWindowController {
     public MainWindowController() {
+    }
+
+    public void loginAction(ActionEvent actionEvent){
+        LogScreenController ctrl = new LogScreenController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/LoginScreen.fxml"));
+        loader.setController(ctrl);
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setTitle("Login");
+        stage.setScene(new Scene(root, 300, 300));
+        stage.setResizable(false);
+        stage.show();
     }
 }
