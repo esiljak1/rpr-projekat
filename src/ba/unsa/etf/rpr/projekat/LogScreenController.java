34d55ff18c4af79d621cf2bcfbb5b61f0f719f1b
Emin Šiljak <esiljak1@etf.unsa.ba>
2020-02-10 23:59:27 +0100

    Added controller for the SignUpScreen

--- src/ba/unsa/etf/rpr/projekat/LogScreenController.java
+++ src/ba/unsa/etf/rpr/projekat/LogScreenController.java
@@ -1,12 +1,17 @@
 package ba.unsa.etf.rpr.projekat;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LogScreenController {
     public TextField fldUsername;
@@ -28,10 +33,27 @@ public class LogScreenController {
             }
         });
 
-        cancelBtn.setOnAction((actionEvent -> {
+        cancelBtn.setOnAction(actionEvent -> {
             Node node = (Node) actionEvent.getSource();
             Stage st = (Stage) node.getScene().getWindow();
             st.close();
-        }));
+        });
+
+        signUpBtn.setOnAction(actionEvent -> {
+            SignUpController ctrl = new SignUpController();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/SignUpScreen.fxml"));
+            loader.setController(ctrl);
+            Parent root = null;
+            try {
+                root = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Stage stage = new Stage();
+            stage.setTitle("Sign up");
+            stage.setScene(new Scene(root, 400, 400));
+            stage.setResizable(false);
+            stage.show();
+        });
     }
 }
