c396ee1e7f5d57065d33294bc61325ee1cb5ae0d
Emin Šiljak <esiljak1@etf.unsa.ba>
2020-02-10 23:53:37 +0100

    Added sign up screen without controller

---
+++ src/ba/unsa/etf/rpr/projekat/SignUpController.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.projekat;
+
+public class SignUpController {
+
+    public SignUpController() {
+    }
+    //todo dodati default sliku za signup
+
+    //todo mrežno programiranje, koristiti giphy za postavljanje slika :)
+}
