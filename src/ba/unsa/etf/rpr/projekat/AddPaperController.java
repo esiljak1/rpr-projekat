f99b20573f7780fd6fe29d625b0f5c95bfdb863d
Emin Šiljak <esiljak1@etf.unsa.ba>
2020-02-13 21:43:52 +0100

    Add file now opens a file chooser

--- src/ba/unsa/etf/rpr/projekat/AddPaperController.java
+++ src/ba/unsa/etf/rpr/projekat/AddPaperController.java
@@ -1,7 +1,26 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class AddPaperController {
+    public Button addFileBtn;
+
     public AddPaperController() {
+    }
+
+    @FXML
+    public void initialize(){
+        addFileBtn.setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text files", "*.txt"));
+            File selectedFile = fileChooser.showOpenDialog(new Stage());
+            //todo dodati selectedFile u files resurse
+        });
     }
 
 }
