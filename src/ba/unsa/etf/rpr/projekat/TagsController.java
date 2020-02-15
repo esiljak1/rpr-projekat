314437b69186779770b507995a5b86e051833230
Emin Šiljak <esiljak1@etf.unsa.ba>
2020-02-15 14:33:33 +0100

    Added ok and cancel button functionalities in Tags window

--- src/ba/unsa/etf/rpr/projekat/TagsController.java
+++ src/ba/unsa/etf/rpr/projekat/TagsController.java
@@ -1,14 +1,31 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 public class TagsController {
     public TextArea tagsTxt;
+
+    private String tags;
 
     public TagsController() {
     }
 
     public String getTags(){
-        return tagsTxt.getText();
+        return tags;
+    }
+
+    public void closeAction(ActionEvent actionEvent){
+        if(((Button) actionEvent.getSource()).getText().equals("Cancel")){
+            tags = null;
+        }else{
+            tags = tagsTxt.getText();
+        }
+        Node node = (Node) actionEvent.getSource();
+        Stage st = (Stage) node.getScene().getWindow();
+        st.close();
     }
 }
