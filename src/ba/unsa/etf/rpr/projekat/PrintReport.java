da18d0b89ef90b3d3af1f62fbf78b7c23d1c521b
Emin Šiljak <esiljak1@etf.unsa.ba>
2020-02-17 18:41:41 +0100

    Added reporting

---
+++ src/ba/unsa/etf/rpr/projekat/PrintReport.java
@@ -0,0 +1,34 @@
+package ba.unsa.etf.rpr.projekat;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.swing.JRViewer;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PrintReport extends JFrame {
+    public void showReport(Connection conn) throws JRException {
+//        String reportSrcFile = getClass().getResource("/jrxml/korisnici.jrxml").getFile();
+//        String reportsDir = getClass().getResource("/jrxml/").getFile();
+
+        String reportSrcFile = "@/../Resources/reports/project.jrxml";
+        String reportsDir = "@/../Resources/reports";
+
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportSrcFile);
+        // Fields for resources path
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("reportsDirPath", reportsDir);
+        ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+        list.add(parameters);
+        JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, conn);
+        JRViewer viewer = new JRViewer(print);
+        viewer.setOpaque(true);
+        viewer.setVisible(true);
+        this.add(viewer);
+        this.setSize(700, 500);
+        this.setVisible(true);
+    }
+}
